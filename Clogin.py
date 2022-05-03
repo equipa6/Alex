e3fb6039f84c30f4bde5b2003d9d1f72116dc893
equipa6 <equip_a6@boscdelacoma.cat>
2022-05-03 20:00:06 +0200

    commit1

--- Clogin.py
+++ Clogin.py
@@ -3,8 +3,17 @@ import socket
 import threading
 
 root = Tk()
-root.geometry("700x400")
+root.geometry("600x300")
+root.resizable(0,0)
+root.title("Inicia sessió")
 
+nom_empresa = Label(root, text="CLOGIN", font=("Calibri", 30))
+nom_empresa.place(x=240, y=0)
 
+nom_usuari = Label(root, text="Nom d'usuari", font=("Calibri", 20))
+nom_usuari.place(x=10, y=90)
+
+contrasenya = Label(root, text="Contrasenya", font=("Calibri", 20))
+contrasenya.place(x=10, y=160)
 
 root.mainloop()
