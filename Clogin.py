052b96891da4ed827a8bc0566322592d947110a8
equipa6 <equip_a6@boscdelacoma.cat>
2022-05-04 19:15:05 +0200

    commit

--- Clogin.py
+++ Clogin.py
@@ -15,21 +15,30 @@ foto = Label(root, image=zoom, bg="#ccdadd")
 foto.place(x=125, y=0)
 
 nom_usuari = Label(root, text="Nom d'usuari", font=("Calibri", 18), bg="#ccdadd")
-nom_usuari.place(x=125, y=150)
+nom_usuari.place(x=128, y=150)
 
 contrasenya = Label(root, text="Contrasenya", font=("Calibri", 18), bg="#ccdadd")
-contrasenya.place(x=125, y=260)
+contrasenya.place(x=130, y=260)
 
 inp_nom = ttk.Entry(root, font=("Calibri", 18), width=28)
-inp_nom.place(x=30, y=200)
+inp_nom.place(x=28, y=200)
 
 inp_contrasenya = ttk.Entry(root, font=("Calibri", 18), width=28)
-inp_contrasenya.place(x=30, y=300)
+inp_contrasenya.place(x=28, y=300)
 inp_contrasenya.config(show="*")
 
 imagen_singup = PhotoImage(file="singup.png")
-img = imagen_singup.subsample(5)
-boton_singup = Button(root, image=img, borderwidth=0, bg="#ccdadd")
+imgagen_singup = imagen_singup.subsample(5)
+boton_singup = Button(root, image=imgagen_singup, borderwidth=0, bg="#ccdadd", cursor="hand2")
 boton_singup.place(x=125, y=370)
+
+lab_register = Label(root, text="No tens compte", font=("Calibri", 14, "underline"), bg="#ccdadd")
+lab_register.place(x=50, y=530)
+
+foto_register = PhotoImage(file="register.png")
+foto_register = foto_register.subsample(2)
+button_register = Button(root, image=foto_register, bg="#ccdadd",borderwidth=0)
+button_register.place(x=175, y=510)
+
 
 root.mainloop()
